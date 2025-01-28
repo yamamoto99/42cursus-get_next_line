92302796aff42208182d4c6c7f765d1a2aad0c12
yamamoto99 <yamamoto1952hkb@gmail.com>
2025-01-28 22:32:28 +0900

    get_next_line: change character type in ft_push_char to unsigned char for consistency

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: masayama <masayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 21:41:13 by masayama          #+#    #+#             */
-/*   Updated: 2025/01/28 22:03:13 by masayama         ###   ########.fr       */
+/*   Updated: 2025/01/28 22:32:20 by masayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void		ft_init_buffer(t_gnl_buffer *buffer);
 int			ft_get_char(int fd);
 void		*ft_memcpy(void *dst, const void *src, size_t n);
 int			ft_expand_buffer(t_gnl_buffer *buffer, size_t new_capacity);
-int			ft_push_char(t_gnl_buffer *buffer, char c);
+int			ft_push_char(t_gnl_buffer *buffer, unsigned char c);
 char		*get_next_line(int fd);
 
 #endif
